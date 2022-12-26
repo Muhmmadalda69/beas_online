dedc31861cf0e24f5b3167f74f552bdced59e4e4
wahyudarajat <Muhammadalda62@gmail.com>
2022-12-26 18:54:06 +0700

    first commit

--- app/src/main/java/com/example/beas/L1Activity.java
+++ app/src/main/java/com/example/beas/L1Activity.java
@@ -1,6 +1,4 @@
 package com.example.beas;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.ContentValues;
@@ -10,13 +8,14 @@ import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.preference.PreferenceManager;
 import android.text.InputType;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.beas.helper.DBHelper;
 
