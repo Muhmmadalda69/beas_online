24c60401272566ea133f309d4c304352e3382838
wahyudarajat <Muhammadalda62@gmail.com>
2023-01-25 15:51:17 +0700

    membuat total skor dari firebase

--- app/src/main/java/com/example/beas/LihatSkorActivity.java
+++ app/src/main/java/com/example/beas/LihatSkorActivity.java
@@ -8,7 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.AutoText;
 import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,11 +18,14 @@ import android.widget.Toast;
 
 import com.example.beas.model.AdapterNilai;
 import com.example.beas.model.Nilai;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -29,24 +34,53 @@ import java.util.Objects;
 
 public class LihatSkorActivity extends AppCompatActivity {
 
+    //FirebaseUser firebaseUser;
+
+    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    String namaUser = Objects.requireNonNull(firebaseUser).getDisplayName();
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference dbSkorRef = database.getReference("db_skor");
+    DatabaseReference dbTotal = database.getReference("db_total");
+
     ListView listView;
+    TextView mySkor;
+    String level1,level2,level3,level4,level5;
     Nilai nilai;
+    int totalNilai = 0;
 
     private ArrayList<Nilai> nilaiArrayList;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lihat_skor);
 
         listView = findViewById(R.id.tv_skor);
+        mySkor = findViewById(R.id.mySkor);
         nilaiArrayList = new ArrayList<>();
 
+        dbSkorRef.child(namaUser).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot nilaiSnapshot : snapshot.getChildren()) {
+                    level1 = nilaiSnapshot.getValue(String.class);
+                    assert level1 != null;
+                    totalNilai += Integer.parseInt(level1);
+                }
+                mySkor.setText(namaUser + " - " + totalNilai);
+                dbTotal.child(namaUser).setValue(totalNilai);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 
+/*
     protected void onStart(){
         super.onStart();
         dbSkorRef.addValueEventListener(new ValueEventListener() {
@@ -69,5 +103,7 @@ public class LihatSkorActivity extends AppCompatActivity {
             }
         });
     }
+
+ */
 
 }//end DisplayList class
